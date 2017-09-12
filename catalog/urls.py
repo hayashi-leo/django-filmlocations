93db73aeae701f7529cd3a7bad18723307428c00
hayashi_leo <hayashi_leo@yahoo.com>
2017-09-11 22:51:29 -0500

    added updateView for listing_detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^$', catalog_views.listing, name='catalog_listing'),
     ### [-\w] regex: create a 'character class', match '-' or any word char
     url(r'^(?P<slug>[-\w]+)$', catalog_views.LocationDetail.as_view(), name='location'),
+    url(r'^(?P<slug>[-\w]+)/update/$', catalog_views.LocationUpdate.as_view(), name='location_update_slug')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- catalog/views.py
+++ catalog/views.py
@@ -2,11 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.http import HttpRequest
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import DetailView
 
 from .models import Location, Image
+
+
+### django generic editing views
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+
 
 def listing(request):
     list = Location.objects.filter(is_visible=True).order_by('-created_date')
@@ -32,3 +39,12 @@ class LocationDetail(DetailView):
         context['images'] = Image.objects.filter(location=self.object.id)
         return context
 
+## see mozilla django-locallibrary tutorial
+class LocationUpdate(UpdateView):
+    model = Location
+    template_name = 'catalog/listing_update.html'
+    fields = '__all__' ## include all fields
+    ## fields = ['summary', 'description'] ## include only these fields
+
+    def get_success_url(self):
+        return reverse('location', kwargs={'slug': self.kwargs['slug']})
